4d7450fcd66ff00460f24cdcd71d80aa9a61e171
river8 <chuan.han.comm@gmail.com>
2019-11-01 18:48:40 +0000

    Add srv-1 update

--- perf_testing/r740/vpp/start_vpp.sh
+++ perf_testing/r740/vpp/start_vpp.sh
@@ -1,8 +1,8 @@
 ifconfig eno2 down
 ip addr flush dev eno2
 
-ifconfig eno3 down
-ip addr flush dev eno3
+ifconfig eno4 down
+ip addr flush dev eno4
 
 service vpp stop
 vpp -c vpp_startup/startup.conf
