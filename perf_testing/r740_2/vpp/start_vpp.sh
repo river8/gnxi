0c12fcbb6a408beb459c75c9f75c9d5872416868
river8 <chuan.han.comm@gmail.com>
2019-11-05 18:49:39 +0000

    srv-2 update

--- perf_testing/r740_2/vpp/start_vpp.sh
+++ perf_testing/r740_2/vpp/start_vpp.sh
@@ -5,4 +5,11 @@ ifconfig eno3 down
 ip addr flush dev eno3
 
 service vpp stop
+
+mkdir -p /tmp/dumps
+sysctl -w debug.exception-trace=1
+sysctl -w kernel.core_pattern="/tmp/dumps/%e-%t"
+ulimit -c unlimited
+echo 2 > /proc/sys/fs/suid_dumpable
+
 vpp -c vpp_startup/startup.conf
