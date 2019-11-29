773c14b6d9ec2cd58b3314e35f843acb5833915e
river8 <chuan.han.comm@gmail.com>
2019-11-29 08:15:42 -0800

    srv-2 update

--- perf_testing/r740_2/vpp/ovs/setup.sh
+++ perf_testing/r740_2/vpp/ovs/setup.sh
@@ -1,5 +1,5 @@
-ip netns add ns1
-ip l set eno2 netns ns1
-ip l set eno3 netns ns1
-ip netns exec ns1 ifconfig eno2 up
-ip netns exec ns1 ifconfig eno3 up
+ip netns add ns2
+ip l set eno2 netns ns2
+ip l set eno3 netns ns2
+ip netns exec ns2 ifconfig eno2 up
+ip netns exec ns2 ifconfig eno3 up
